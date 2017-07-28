8283a995ce069e8ac36d878da3f0e78d3c680344
slumber1122 <slumber1122@gmail.com>
2017-07-28 17:41:32 +0800

    add some logic

--- converter/converter.go
+++ converter/converter.go
@@ -1,6 +1,11 @@
 package converter
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
 // Converter converter
 type Converter struct {
@@ -11,7 +16,56 @@ type Converter struct {
 // Run convert
 func (c *Converter) Run() {
 	fmt.Println("begin convert website: ", c.source)
+	c.convert(0, c.source, c.dest)
+}
 
+func (c *Converter) genNewDestDir(dest string) {
+
+}
+
+func (c *Converter) genNewRelativeFile(depth int, path string, dest string) {
+	ss := strings.Split(path, "/")
+	destfile := filepath.Join(dest, ss[len(ss)-1])
+	if !isFileExist(destfile) {
+		newfile, err := os.Create(destfile)
+		if err != nil {
+
+		}
+
+	}
+}
+
+func (c *Converter) convert(depth int, source string, dest string) {
+	c.genNewDestDir(dest)
+	walkFunc := func(source string, f os.FileInfo, err error) error {
+		if f == nil {
+			return err
+		}
+		if f.IsDir() {
+			dest = filepath.Join(dest, f.Name())
+			c.convert(depth+1, source, dest)
+		}
+		if isHtmlFile(f.Name()) {
+			c.genNewRelativeFile(depth, source, dest)
+		}
+		return nil
+	}
+	err := filepath.Walk(c.source, walkFunc)
+	if err != nil {
+		fmt.Printf("filepath.Walk() return %v\n", err)
+	}
+}
+
+func isHtmlFile(path string) bool {
+	path = strings.ToLower(path)
+	if strings.HasSuffix(path, "html") || strings.HasSuffix(path, "htm") {
+		return true
+	}
+	return false
+}
+
+func isFileExist(path string) bool {
+	return false
 }
 
 // NewConverter new converter
--- main.go
+++ main.go
@@ -3,17 +3,33 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/slumber1122/abs-relative-converter/converter"
 )
 
 func main() {
-	source := flag.String("source", "", "the website diretory which you want to convert")
-	dest := flag.String("dest", "", "the converted website diretory")
-	if *source == "" || *dest == "" {
+	source := *flag.String("s", "", "the website diretory which you want to convert")
+	dest := *flag.String("d", "", "the converted website diretory")
+	flag.Parse()
+	fmt.Println("source: ", source, dest)
+	if source == "" || dest == "" {
 		fmt.Println("You need supply website source diretory and dest diretory")
 		return
 	}
-	converter := converter.NewConverter(*source, *dest)
+	source = convertToAbsPath(source)
+	dest = convertToAbsPath(dest)
+
+	converter := converter.NewConverter(source, dest)
 	converter.Run()
+}
+
+func convertToAbsPath(path string) string {
+	if strings.HasPrefix(path, "/") {
+		return path
+	}
+	execDirAbsPath, _ := os.Getwd()
+	return filepath.Join(execDirAbsPath, path)
 }
